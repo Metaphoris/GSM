e4de4ca28aa8830eb40fc5ec42f6a9909fa284d1
Metaphoris <EYWdiz1t|akr>
2018-08-29 22:43:33 +0300

    First commit

---
+++ GSM/Program.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections;
+
+namespace GSM
+{
+    public static class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        //Кодирование
+        public static byte[] Encode(string message)
+        {
+            //Определяем индексы каждого символа сообщения в GSM-алфавите
+            string alphabet = "@£$¥èéùìòÇ\nØø\rÅå" + "Δ_ΦΓΛΩΠΨΣΘΞ\x1bÆæßÉ"
+                            + " !\"#¤%&`()*+,-./" + "0123456789:;<=>?"
+                            + "¡ABCDEFGHIJKLMNO" + "PQRSTUVWXYZÄÖÑÜ§"
+                            + "¿abcdefghijklmno" + "pqrstuvwxyzäöñüà";
+
+            char[] message_array = message.ToCharArray();
+            byte[] code_array = new byte[message.Length];
+
+            for (Byte i = 0; i < message_array.Length; i++)
+            {
+                code_array[i] = Convert.ToByte(alphabet.IndexOf(message_array[i]));
+            }
+
+            //Конвертируем в массив бит
+            BitArray unpacked = new BitArray(code_array);
+
+            //Определяем число бит, достаточное для хранения в сжатом виде
+            int packedSize = code_array.Length * 7;
+
+            //При необходимости, выравниваем размер будущего массива,
+            //делая его кратным 8
+            bool b = false;
+            while (b == false)
+            {
+                if (packedSize % 8 == 0)
+                    b = true;
+                else
+                    packedSize++;
+            }
+            BitArray packed = new BitArray(packedSize);
+
+            //Упаковка
+            int j = 0;
+            for (int i = 0; i < unpacked.Count; i++)
+            {
+                if ((i + 1) % 8 != 0)
+                {
+                    packed[j] = unpacked[i];
+                    j++;
+                }
+            }
+
+            //Конвертируем упаковыннй массив бит в массив байт 
+            byte[] encryptedMessage = new byte[packed.Length / 8];
+            packed.CopyTo(encryptedMessage, 0);
+
+            //Возвращаем результат
+            return encryptedMessage;
+        }
+
+        //Декодирование
+        public static string Decode(byte[] paylod)
+        {
+            string alphabet = "@£$¥èéùìòÇ\nØø\rÅå" + "Δ_ΦΓΛΩΠΨΣΘΞ\x1bÆæßÉ"
+                            + " !\"#¤%&`()*+,-./" + "0123456789:;<=>?"
+                            + "¡ABCDEFGHIJKLMNO" + "PQRSTUVWXYZÄÖÑÜ§"
+                            + "¿abcdefghijklmno" + "pqrstuvwxyzäöñüà";
+
+            //Конвертируем массив байт в массив бит
+            BitArray packed = new BitArray(paylod);
+
+            //Определяем размер массива бит после распаковки
+            int unpackedSize = ((paylod.Length * 8) - (paylod.Length * 8 % 7)) * 8 / 7;
+            BitArray unpacked = new BitArray(unpackedSize);
+
+            //Распаковка
+            int j = 0;
+            for (int i = 0; i < (packed.Count - (packed.Count % 7)); i++)
+            {
+                if ((j + 1) % 8 == 0)
+                {
+                    unpacked[j] = false;
+                    j++;
+                }
+                unpacked[j] = packed[i];
+                j++;
+            }
+
+            //Конвертируем массив бит в массив индексов
+            byte[] code_array = new byte[unpacked.Length / 8];
+            unpacked.CopyTo(code_array, 0);
+
+            //Восстанавливаем сообщение по индексу
+            string message = "";
+            for (int i = 0; i < code_array.Length; i++)
+            {
+                message += alphabet[code_array[i]];
+            }
+
+            return message;
+        }
+    }
+}
---
+++ GSM_Tests/UnitTest1.cs
@@ -0,0 +1,64 @@
+using System;
+using Xunit;
+
+namespace GSM_Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void EncodeAlphabetTest()
+        {
+            byte[] alphabet = {128, 128,  96,  64,  40,  24,  14, 136, 132,  98, 193, 104,  56,  30, 144, 136,
+                               100,  66, 169,  88,  46, 152, 140, 102, 195, 233, 120,  62, 160, 144, 104,  68,
+                                42, 153,  78, 168, 148, 106, 197, 106, 185,  94, 176, 152, 108,  70, 171, 217,
+                               110, 184, 156, 110, 199, 235, 249, 126, 192, 160, 112,  72,  44,  26, 143, 200,
+                               164, 114, 201, 108,  58, 159, 208, 168, 116,  74, 173,  90, 175, 216, 172, 118,
+                               203, 237, 122, 191, 224, 176, 120,  76,  46, 155, 207, 232, 180, 122, 205, 110,
+                               187, 223, 240, 184, 124,  78, 175, 219, 239, 248, 188, 126, 207, 239, 251, 255};
+            
+            Assert.Equal(GSM.Program.Encode("@£$¥èéùìòÇ\nØø\rÅå" + "Δ_ΦΓΛΩΠΨΣΘΞ\x1bÆæßÉ"
+                            + " !\"#¤%&`()*+,-./" + "0123456789:;<=>?"
+                            + "¡ABCDEFGHIJKLMNO" + "PQRSTUVWXYZÄÖÑÜ§"
+                            + "¿abcdefghijklmno" + "pqrstuvwxyzäöñüà"), alphabet);
+        }
+
+        [Fact]
+        public void DecodeAlphabetTest()
+        {
+            byte[] alphabet = {128, 128,  96,  64,  40,  24,  14, 136, 132,  98, 193, 104,  56,  30, 144, 136,
+                               100,  66, 169,  88,  46, 152, 140, 102, 195, 233, 120,  62, 160, 144, 104,  68,
+                                42, 153,  78, 168, 148, 106, 197, 106, 185,  94, 176, 152, 108,  70, 171, 217,
+                               110, 184, 156, 110, 199, 235, 249, 126, 192, 160, 112,  72,  44,  26, 143, 200,
+                               164, 114, 201, 108,  58, 159, 208, 168, 116,  74, 173,  90, 175, 216, 172, 118,
+                               203, 237, 122, 191, 224, 176, 120,  76,  46, 155, 207, 232, 180, 122, 205, 110,
+                               187, 223, 240, 184, 124,  78, 175, 219, 239, 248, 188, 126, 207, 239, 251, 255};
+
+            string alphabetString = "@£$¥èéùìòÇ\nØø\rÅå" + "Δ_ΦΓΛΩΠΨΣΘΞ\x1bÆæßÉ"
+                                  + " !\"#¤%&`()*+,-./" + "0123456789:;<=>?"
+                                  + "¡ABCDEFGHIJKLMNO" + "PQRSTUVWXYZÄÖÑÜ§"
+                                  + "¿abcdefghijklmno" + "pqrstuvwxyzäöñüà";
+            
+            Assert.Equal(GSM.Program.Decode(alphabet), alphabetString);
+        }
+
+        [Fact]
+        public void CheckRandomMessage()
+        {
+            string alphabet = "@£$¥èéùìòÇ\nØø\rÅå" + "Δ_ΦΓΛΩΠΨΣΘΞ\x1bÆæßÉ"
+                            + " !\"#¤%&`()*+,-./" + "0123456789:;<=>?"
+                            + "¡ABCDEFGHIJKLMNO" + "PQRSTUVWXYZÄÖÑÜ§"
+                            + "¿abcdefghijklmno" + "pqrstuvwxyzäöñüà";
+
+            Random rnd = new Random();
+            byte messageLength = Convert.ToByte(rnd.Next(0, 119));
+            string message = "";
+
+            for (byte i = 0; i < messageLength; i++)
+            {
+                message += alphabet[rnd.Next(0, 119)];
+            }
+
+            Assert.Equal(message, GSM.Program.Decode(GSM.Program.Encode(message)));
+        }
+    }
+}
